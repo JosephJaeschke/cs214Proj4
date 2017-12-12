1c5240b2f28993b5984cd7f9d384a7952c9d9268
JosephJaeschke <joeyj987@gmail.com>
2017-12-11 20:14:09 -0500

    multithreaded server

--- client_test.c
+++ client_test.c
@@ -14,49 +14,46 @@
 
 int main(int argc,char **argv)
 {
-    int sockfd,n;
+	int sockfd,n;
 	int sizeof_string = 10000;
-    char * sendline = malloc(sizeof_string);
-    
+	char * sendline = malloc(sizeof_string);
 	struct sockaddr_in servaddr;
+    
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+    	bzero(&servaddr,sizeof servaddr);
  
-    sockfd=socket(AF_INET,SOCK_STREAM,0);
-    bzero(&servaddr,sizeof servaddr);
- 
-    int port = atoi(argv[2]);
+    	int port = atoi(argv[2]);
 	servaddr.sin_family=AF_INET;
 
-    servaddr.sin_port=htons(port);
- 
-    inet_pton(AF_INET,"127.0.0.1",&(servaddr.sin_addr));
- 
-    if(connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) == -1){
+    	servaddr.sin_port=htons(port);
+    	inet_pton(AF_INET,argv[1],&(servaddr.sin_addr));
+    
+    	if(connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) == -1)
+	{
 		printf("ERROR : Failed to connect to server\n");
 		exit(EXIT_FAILURE);
 	}
 
-
 	char * filename1 = malloc(100);
 	
-	strncpy(filename1, argv[1], strlen(argv[1]));
-	filename1[strlen(argv[1]) + 1] = '\0';
-
+	strncpy(filename1, argv[3], strlen(argv[3]));
+	filename1[strlen(argv[3]) + 1] = '\0';
 	printf("%s\n", filename1);
 	FILE * fp = fopen(filename1, "r");
 	int row_position = 0;
 	int j = 0;
 	int file_count = 0;
 	int i = 0;
-
 	char c = getc(fp);
-	
-	while (c != EOF) {
-		if(c == '\n'){
+	while (c != EOF)
+	{
+		if(c == '\n')
+		{
 			file_count++;
 		}
 		i++;
 		c = getc(fp);
-    }
+    	}
 
 
 	fclose(fp);
@@ -66,64 +63,59 @@ int main(int argc,char **argv)
 	
 	FILE * fp1 = fopen(filename1, "r");
 	
-	if(fp1 == NULL){
+	if(fp1 == NULL)
+	{
+		exit(1);
 	}
+
 	c = getc(fp1);
 	
 	i=0;
-	while (c != EOF) {
+	while (c != EOF)
+	{
 		str_file[i] = c;
 		i++;
 		c = getc(fp1);
-    }
-   
+   	}
 	if(fp1 != NULL)
 	{
 		fclose(fp1);	
 	}
-	if(filename1 != NULL){
-
-	free(filename1);
+	if(filename1 != NULL)
+	{
+		free(filename1);
 	}	
 	
 	
 	str_file[i] = '\0';	
 	
 	int sentn = htonl(file_count);
-	
 	char * recvline = malloc(100);
 	//read(sockfd,recvline,100);
-        	
-	write(sockfd,&sentn ,sizeof(sentn));   	
-
+	printf("first write -> %d\n",ntohl(sentn));
+	write(sockfd,&sentn,sizeof(sentn));   	
+	read(sockfd,recvline,100);
+	printf("second write -> %d\n",i);
+	write(sockfd,&i,sizeof(i));
+	read(sockfd,recvline,100);
 	int index1 = 0;
 	int index2 = 0;
 
 	
-    for(j = 0; j < file_count; j++)
-    {
-        
+	for(j = 0; j < file_count; j++)
+    	{
 		//sleep(1);
 		bzero(sendline, 10000);
-		
-		while(str_file[index2] != '\n'){
+		while(str_file[index2] != '\n')
+		{
 			index2++;
 		}
 		index2++;
-
 		strncpy(sendline,str_file+index1, index2 - index1);
-
-        //fgets(sendline,100,stdin); /*stdin = 0 , for standard input */
-		//printf("%s", sendline); 
-        
 		write(sockfd,sendline,strlen(sendline)+1);
-       	
 		index1 = index2; 
-		
 		read(sockfd,recvline,100);
-        //printf("%s",recvline);
-    }
-	
+	}
 	//read(sockfd,recvline,100);    
 	printf("\nFile Sent\n");
 	return 0; 
--- serv_test.c
+++ serv_test.c
@@ -7,31 +7,93 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
 
+pthread_mutex_t m;
+pthread_t* threadIDs;
+int threadIndex=0;
+struct sockaddr_in servaddr; //init stuff
+int listen_fd, comm_fd;
 
-
-void write_test(FILE * fp, char * str){ //test function to be replaced with merge/etc
+void write_test(FILE * fp, char * str) //test function to be replaced with merge/etc
+{
 	//printf("%s", str);
 	fprintf(fp, "%s", str);
 	fflush(fp);
 }
 
+void* rec(void* args)
+{	
+	char * str = malloc(10000);
+   	int size = 0;
+	int fileSize=0;
+	char* recv=malloc(100);
+	strcpy(recv,"hello");
+	if (read(comm_fd, &size, sizeof(size)) == 0) //get size lines of file
+	{
+		printf("[-] Disconnected from client %d\n", listen_fd); //to be changed to ip?
+		free(str);
+		return "no";
+	}
+	write(comm_fd,"q",2);
+	printf("[+] Connect to client %d\n", listen_fd); //to be changed to ip?
+	if(ntohl(size)<0)
+	{
+		//this is not a file
+		printf("dump\n");
+		//do the dump thing and send over sorted of all files
+		return "yo";
+	}
+	if (read(comm_fd, &fileSize, sizeof(fileSize)) == 0) //get size lines of file
+	{
+		printf("[-] Disconnected from client %d\n", listen_fd); //to be changed to ip?
+		free(str);
+		return "no";
+	}
+	write(comm_fd,"q",2);
+	FILE* out_file = fopen("files_sorted.csv", "w");
+	char* file=malloc(size);
+	char* whole=malloc(fileSize);	
+	strcpy(recv,"hello");
+	int j = 0;
+	int len=0;
+	for (j = 0; j < ntohl(size); j++)
+    	{
+ 		
+		bzero(str, 10000); 
+       		if(read(comm_fd,str,10000) == 0) //go through each line of csv, and get the line
+		{
+			printf("[-] Disconnected from client %d\n", listen_fd); //to be changed to ip?
+ 			comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
+			continue;
+		}
+		else
+		{
+			len+=sprintf(whole+len,"%s",str);
+ 			fflush(out_file);
+			write(comm_fd, recv, strlen(recv)+1); //send back a signal to show that it's done recieving data(lets it be in order)	
+		}
+		 
+    	}
+	fprintf(out_file,"%s",whole);
+ 	fclose(out_file);
+	free(str);
+	free(file);
+	free(whole);
+	return "no";
+}
 
-int main(int argc, char ** argv)
+int main(int argc, char** argv)
 {
- 
-    char * str = malloc(10000);
-    int listen_fd, comm_fd;
- 
-    struct sockaddr_in servaddr; //init stuff
+	threadIDs=malloc(sizeof(pthread_t)*5); //change 5 to how many files  
  
  
-    bzero( &servaddr, sizeof(servaddr)); //zero at addresse
+    	bzero( &servaddr, sizeof(servaddr)); //zero at addresse
  
-    //int port = atoi(argv[1]); 
+    	//int port = atoi(argv[1]); 
 	
 	
-	if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) <= 0) //check if sockets being used
+	if((listen_fd=socket(AF_INET,SOCK_STREAM,0))<=0) //check if sockets being used
 	{
 		// print error message with perror()
 		perror("socket");
@@ -41,68 +103,35 @@ int main(int argc, char ** argv)
 
 
 	servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htons(INADDR_ANY);
+	servaddr.sin_addr.s_addr = htons(INADDR_ANY);
 	servaddr.sin_port = htons(0); //set to a random port which isn't being used
  
-    bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)); //bind program to that port
+    	bind(listen_fd, (struct sockaddr *) &servaddr, sizeof(servaddr)); //bind program to that port
  	
 	socklen_t len = sizeof(servaddr);
 	
-	if (getsockname(listen_fd, (struct sockaddr *)&servaddr, &len) == -1) {
-		    perror("getsockname");
-			    return 0;
+	if (getsockname(listen_fd, (struct sockaddr *)&servaddr, &len) == -1)
+	{
+		perror("getsockname");
+		return 0;
 	} //checl of socket binded correctly
 
 	printf("Running On Port Number: %d\n", ntohs(servaddr.sin_port)); //get port
 
-    listen(listen_fd, 10); //allow for max 10 connections (not sure how it changes threads)
+  	listen(listen_fd, 10); //allow for max 10 connections (not sure how it changes threads)
  
-    comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL); //start listening on connection 
- 
-    int size = 0;
 
-	char * recv = malloc(100);
-	strcpy(recv, "hello");
-	
-
-	FILE * out_file = fopen("files_sorted", "w"); //file to write to
-	fflush(out_file);
-	fclose(out_file);
-
-	while (1) {
-
-	   	size = 0;
-		
-		if (read(comm_fd, &size , sizeof(size)) == 0){ //get size lines of file
-			printf("[-] Disconnected from client %d\n", listen_fd); //to be changed to ip?
- 				comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
-				continue;	
+    	while(comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL)) //start listening on connection 
+	{
+		void* state;
+		pthread_t index=threadIDs[threadIndex++];
+		pthread_create(&index,NULL,&rec,NULL);
+		pthread_join(index,&state);
+		char* d=(char*)state;
+		if(d[0]=='y')
+		{
+			break;
 		}
-
-		printf("[+] Connect to client %d\n", listen_fd); //to be changed to ip?
-		
-		out_file = fopen("files_sorted", "w");
-
-	
-		int j = 0;
-		for (j = 0; j < ntohl(size); j++)
-	    {
-	 		
-			bzero(str, 10000); 
-			 
-    	    if(read(comm_fd,str,10000) == 0) { //go through each line of csv, and get the line
-				printf("[-] Disconnected from client %d\n", listen_fd); //to be changed to ip?
- 				comm_fd = accept(listen_fd, (struct sockaddr*) NULL, NULL);
-				continue;
-			}
-			else {
-    	    	write_test(out_file, str);
- 				fflush(out_file);
-				write(comm_fd, recv, strlen(recv)+1); //send back a signal to show that it's done recieving data(lets it be in order)	
-			}
-		 
-   	 }
-	 fclose(out_file);
 	}
 	
 	printf("\n");
